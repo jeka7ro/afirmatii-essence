3fb78a0d0166ec802237097d1ce523f7c5186eb1
Eugeniu Cazmal <eugeniucazmal@gmail.com>
2025-10-27 18:44:41 +0200

    Switch to Railway deployment - Render out of minutes

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
 from datetime import datetime
 
 app = Flask(__name__)
@@ -523,4 +524,5 @@ def get_all_users_admin():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=False)
