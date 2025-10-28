90afeb70f2b53e158a0cd7d5fdbc53c145a89e57
Eugeniu Cazmal <eugeniucazmal@gmail.com>
2025-10-28 13:22:17 +0200

    Fix: Add missing time import

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 import os
+import time
 from datetime import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
